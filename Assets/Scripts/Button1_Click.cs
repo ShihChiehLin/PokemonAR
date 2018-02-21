8379fbd9f1b00356d8b6fedbc8fb84747a5b28b1
Shih-Chieh Lin <shihchiehlin@my.unt.edu>
2018-02-21 11:11:29 +0800

    first
    first time

---
+++ Assets/Scripts/Button1_Click.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1_Click : MonoBehaviour {
+    public Button mButton;
+    
+	void Start () {
+        
+        Button btn = mButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+    {
+        Application.LoadLevel(1);
+        
+    }
+	
+}
---
+++ Assets/Scripts/ButtonQuit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonQuit : MonoBehaviour
+{
+  
+    public void doquit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
